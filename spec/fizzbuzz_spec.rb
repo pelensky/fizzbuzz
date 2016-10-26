e32454b0648b53bafbc29b5c7a7972ea08258b90
Dan Pelensky <dan@pelensky.com>
2016-10-26 14:34:45 +0100

    Tests finished with scenarios and program working

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,26 @@
 require "fizzbuzz"
 
-describe 'fizzbuzz' do #given fizzbuzz method
-  it 'returns "fizz" when passed 3' do #when we pass a number (3)
-    expect(fizzbuzz(3)).to eq 'fizz' #then it gets replaced with fizz
+describe 'fizzbuzz' do
+  it 'returns "Fizz" when passed 3' do 
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+end
+
+
+describe 'fizzbuzz' do
+  it "returns 'FizzBuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns itself when it is neither a multiple of 3, 5 or 15' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
