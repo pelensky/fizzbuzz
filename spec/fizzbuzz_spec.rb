b03059d1e6b984200c5de1a772bdf8e770bac0a9
Dan Pelensky <dan@pelensky.com>
2016-10-26 13:15:23 +0100

    added comments to the fizzbuzz_spec.rb file and created fizzbuzz.rb

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,7 @@
+require "./lib/fizzbuzz"
+
+describe 'fizzbuzz' do #given fizzbuzz method
+  it 'returns "fizz" when passed 3' do #when we pass a number (3)
+    expect(fizzbuzz(3)).to eq 'fizz' #then it gets replaced with fizz
+  end
+end
